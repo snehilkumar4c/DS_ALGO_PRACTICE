cb39739d6f48acba011a83407639bff69717a4c5
Snehil kumar <snehilkumar4c@gmail.com>
2022-08-28 18:43:17 +0530

    iterative_power

--- iterative_power.cpp
+++ iterative_power.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
 
 using namespace std;
-
-int iterativepower(int a, int n) {
+// time complexity for this function is O(log(n)) and auxiliary space is O(1).
+int iterativepower(int x, int n) {
     int res = 1;
 
     while(n > 0) {
-        if(n % 2 != 0) {
-            res = res * a;
-            a= a * a;
-            n = n / 2;
-        }
+        if(n % 2 != 0) 
+            res = res * x;
+        x = x * x;
+        n = n / 2;
     }
-    return res;
+    cout<<res;
 }
 
 int main() {
@@ -22,6 +21,6 @@ int main() {
     cout<<"enter the number and it's computing power: ";
     cin>>a>>n;
 
-    cout<<"the result is: "<<iterativepower(a,n);
+    iterativepower(a,n);
     return 0;
 }
